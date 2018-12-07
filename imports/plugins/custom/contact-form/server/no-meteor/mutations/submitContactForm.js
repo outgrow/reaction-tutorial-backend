f89faab24d22b94df1e6f63d75cb2d0f2f340278
Loan Laux <contact@loanlaux.fr>
2018-12-07 15:49:09 +0100

    Send email on mutation called

--- imports/plugins/custom/contact-form/server/no-meteor/mutations/submitContactForm.js
+++ imports/plugins/custom/contact-form/server/no-meteor/mutations/submitContactForm.js
@@ -1,6 +1,14 @@
 import Logger from "@reactioncommerce/logger";
+import Reaction from "/imports/plugins/core/core/server/Reaction";
 
 export default async function submitContactForm(context, input) {
   Logger.info("submitContactForm was called with input", input);
-  return false;
+
+  return Reaction.Email.send({
+    from: Reaction.getShopEmail(),
+    replyTo: input.email,
+    to: Reaction.getShopEmail(),
+    subject: `Contact form${input.subject ? ` — ${input.subject}` : ""}`,
+    html: input.message
+  });
 }
