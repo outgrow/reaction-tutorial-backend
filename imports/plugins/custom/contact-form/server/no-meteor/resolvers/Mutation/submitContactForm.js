936a59bdcc973c5d6bba932aa7dd5d43f738108e
Loan Laux <loan@outgrow.io>
2018-12-10 17:34:11 +0100

    Clean up schema

--- imports/plugins/custom/contact-form/server/no-meteor/resolvers/Mutation/submitContactForm.js
+++ imports/plugins/custom/contact-form/server/no-meteor/resolvers/Mutation/submitContactForm.js
@@ -1,10 +1,9 @@
 export default async function submitContactForm(parentResult, { input }, context) {
   const { clientMutationId = null } = input;
 
-  const hasError = await context.mutations.submitContactForm(context, input);
+  await context.mutations.submitContactForm(context, input);
 
   return {
-    hasError,
     clientMutationId
   }
 }
