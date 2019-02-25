33b5589d891c09f19143d2c48286209484a70d90
Loan Laux <loan@outgrow.io>
2019-02-25 18:27:12 +0400

    Add form and validator function

--- imports/plugins/custom/retailers/client/components/RetailerDashboard.js
+++ imports/plugins/custom/retailers/client/components/RetailerDashboard.js
@@ -1,10 +1,72 @@
 import React, { Component } from "react";
+import { Form } from "reacto-form";
+import Button from "@reactioncommerce/components/Button/v1";
+import Checkbox from "@reactioncommerce/components/Checkbox/v1";
+import ErrorsBlock from "@reactioncommerce/components/ErrorsBlock/v1";
+import Field from "@reactioncommerce/components/Field/v1";
+import TextInput from "@reactioncommerce/components/TextInput/v1";
 import { Card, CardHeader, CardBody, CardGroup, ListItem } from "/imports/plugins/core/ui/client/components";
 import { SortableTable } from "/imports/plugins/core/ui/client/components";
 
 class RetailerDashboard extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      newRetailer: {}
+    };
+
+    this.form = {};
+  }
+
+  handleFormSubmit = (values) => {
+    console.log(values);
+  };
+
+  handleFormValidate = async (fields) => {
+    const errors = [];
+
+    if (!fields.name || fields.name.length <= 0) {
+      errors.push({
+        message: "Retailer name is required.",
+        name: "name"
+      });
+    }
+
+    if (!fields.latitude || fields.latitude.length <= 0) {
+      errors.push({
+        message: "Latitude is required.",
+        name: "latitude"
+      });
+    }
+
+    if (!fields.longitude || fields.longitude.length <= 0) {
+      errors.push({
+        message: "Longitude is required.",
+        name: "longitude"
+      });
+    }
+
+    if (fields.latitude && fields.latitude.length > 0 && isNaN(parseFloat(fields.latitude))) {
+      errors.push({
+        message: "Latitude has to be a number.",
+        name: "latitude"
+      });
+    }
+
+    if (fields.longitude && fields.longitude.length > 0 && isNaN(parseFloat(fields.longitude))) {
+      errors.push({
+        message: "Longitude has to be a number.",
+        name: "longitude"
+      });
+    }
+
+    return errors;
+  };
+
   render() {
     const { retailers } = this.props;
+
     return (
       <div>
         <SortableTable
@@ -24,7 +86,30 @@ class RetailerDashboard extends Component {
         <Card>
           <CardHeader title={"Add retailers"} />
           <CardBody padded>
-            <h2>Form goes here</h2>
+            <Form
+              ref={(formRef) => { this.form = formRef; }}
+              onSubmit={this.handleFormSubmit}
+              validator={this.handleFormValidate}
+              value={this.state.newRetailer}
+            >
+              <Field name="name" label="Name" labelFor="name-input">
+                <TextInput id="name-input" name="name" />
+                <ErrorsBlock names={["name"]} />
+              </Field>
+              <Field name="latitude" label="Latitude" labelFor="latitude-input">
+                <TextInput id="latitude-input" name="latitude" />
+                <ErrorsBlock names={["latitude"]} />
+              </Field>
+              <Field name="longitude" label="Longitude" labelFor="longitude-input">
+                <TextInput id="longitude-input" name="longitude" />
+                <ErrorsBlock names={["longitude"]} />
+              </Field>
+              <Field name="enabled">
+                <Checkbox name="enabled" label="Enabled" />
+                <ErrorsBlock names={["enabled"]} />
+              </Field>
+              <Button onClick={() => { this.form.submit(); }}>Save</Button>
+            </Form>
           </CardBody>
         </Card>
       </div>
