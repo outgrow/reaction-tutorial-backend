cd753909869e4cf7a70718f5e41546e0f1017677
Loan Laux <loan@outgrow.io>
2019-02-26 10:34:53 +0400

    Update JSDoc

--- imports/plugins/custom/retailers/server/no-meteor/queries/retailers.js
+++ imports/plugins/custom/retailers/server/no-meteor/queries/retailers.js
@@ -1,3 +1,10 @@
+/**
+ * @method addRetailer
+ * @summary Adds a new retailer to the `Retailers` collection
+ * @param {Object} input - the mutation's arguments
+ * @param {Object} context - an object containing the per-request state
+ * @return {Promise<Object>} the list of retailers as an array
+ */
 export default function(context) {
   const retailerCollection = context.collections.Retailers;
 
--- imports/plugins/custom/retailers/server/no-meteor/resolvers/Mutation/addRetailer.js
+++ imports/plugins/custom/retailers/server/no-meteor/resolvers/Mutation/addRetailer.js
@@ -9,7 +9,7 @@ import Logger from "@reactioncommerce/logger";
  * @param {Object} args.input - an object of all mutation arguments that were sent by the client
  * @param {String} [args.input.clientMutationId] - An optional string identifying the mutation call
  * @param {Object} context - an object containing the per-request state
- * @return {Promise<Object>} CreateSomethingPayload
+ * @return {Promise<Object>} AddRetailerPayload
  */
 export default async function addRetailer(parentResult, { input }, context) {
   const { clientMutationId = null } = input;
