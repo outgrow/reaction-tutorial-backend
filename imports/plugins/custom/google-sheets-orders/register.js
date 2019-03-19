ea353bb186fc5c0becf8e2c557e7fe1eb18f654d
Loan Laux <loan@outgrow.io>
2019-03-19 16:00:41 +0100

    Add registerPackage

---
+++ imports/plugins/custom/google-sheets-orders/register.js
@@ -0,0 +1,8 @@
+import Reaction from "/imports/plugins/core/core/server/Reaction";
+
+Reaction.registerPackage({
+  label: "Google Sheets Order Export",
+  name: "google-sheets-orders",
+  icon: "fa fa-table",
+  autoEnable: true
+});
