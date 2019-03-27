f1cb4468b6fcb0af464a6f8bb6370968128c536b
Loan Laux <loan@outgrow.io>
2019-03-27 23:56:11 +0400

    Generate and return authentication URL

--- imports/plugins/custom/google-sheets-orders/server/no-meteor/queries/googleAuthenticationUrl.js
+++ imports/plugins/custom/google-sheets-orders/server/no-meteor/queries/googleAuthenticationUrl.js
@@ -1,4 +1,7 @@
+import { google } from "googleapis";
+import Reaction from "/imports/plugins/core/core/server/Reaction";
 
+const SCOPES = ["https://www.googleapis.com/auth/spreadsheets"];
 
 /**
  * @name googleAuthenticationUrl
@@ -9,6 +12,19 @@
  * @param {Object} params - request parameters
  * @return {Promise<Object>} - Object containing the URL to redirect the user to
  */
-export default function googleAuthenticationUrl(context, params) {
-  return { url: "nope" };
+export default async function googleAuthenticationUrl(context, params) {
+  const { collections } = context;
+  const { Packages } = collections;
+
+  const packageInfo = await Packages.findOne({ name: "google-sheets-orders" });
+  const redirectUrl = `${Reaction.absoluteUrl()}operator/google-sheets/autorize?token=`;
+
+  const client = new google.auth.OAuth2(packageInfo.settings.clientId, packageInfo.settings.clientSecret, redirectUrl);
+
+  const url = client.generateAuthUrl({
+    access_type: "online",
+    scope: SCOPES,
+  });
+
+  return { url };
 }
